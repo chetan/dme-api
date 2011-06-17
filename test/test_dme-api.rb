fea8ed5d66a67934574f0342c240e1401063c7d2
Chetan Sarva <chetan@betteradvertising.com>
2011-06-17 18:20:17 -0400

    added a test for create & delete methods

--- test/test_dme-api.rb
+++ test/test_dme-api.rb
@@ -3,7 +3,7 @@ require 'helper'
 class TestDmeApi < Test::Unit::TestCase
 
   def setup
-    @api = ""
+    @api    = ""
     @secret = ""
     @domain = ""
   end
@@ -34,6 +34,22 @@ class TestDmeApi < Test::Unit::TestCase
     rescue => ex
       assert ex.kind_of? RestClient::ResourceNotFound
     end
+  end
+
+  def test_create_and_delete
+    dme = DME.new(@api, @secret, @domain)
+    rec = { "name"        => "testtesttest",
+            "type"        => "A",
+            "data"        => "4.2.2.2",
+            "gtdLocation" => "DEFAULT",
+            "ttl"         => 300 }
+    dme.create(rec)
+
+    rec = dme.get("testtesttest")
+    assert rec
+
+    dme.delete(rec["id"])
+    assert !dme.get("testtesttest")
   end
 
 end
